c0b3e509b22f411d5af6cbe691d561f4cf482cf3
Priyanka-GD <PriEngg@1994>
2020-05-20 22:36:47 +0530

    Initial commit #1

---
+++ src/test/java/com/springframework/breweryservice/BreweryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.breweryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
